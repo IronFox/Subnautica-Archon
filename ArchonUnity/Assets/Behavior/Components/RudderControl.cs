22b3a4816c69c4ace96e38ece742d9892820ee32
IronFox <github@portal.delta-works.org>
2025-03-27 17:32:44 +0100

    Make rudders turn by intention instead of inertia

--- ArchonUnity/Assets/Behavior/Components/RudderControl.cs
+++ ArchonUnity/Assets/Behavior/Components/RudderControl.cs
@@ -1,33 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RudderControl : MonoBehaviour
 {
-    private Vector3 lastPosition;
+    //private Vector3 lastPosition;
     // Start is called before the first frame update
     void Start()
     {
-        lastPosition = transform.parent.position;
+        //lastPosition = transform.parent.position;
     }
 
     private const float maxDegPerSecond = 60;
+
+    public ProjectedMotionSpace Intention { get; private set; }
 
     // Update is called once per frame
     void Update()
     {
         var t = transform.parent;
         var newPosition = t.position;
-        if (Time.deltaTime > 0)
+        if (Intention != null)
         {
-            var velocity = (newPosition - lastPosition) / Time.deltaTime;
+            //var velocity = (newPosition - lastPosition) / Time.deltaTime;
+            var future = Intention.Predict(t.position);
+            var velocity = future - t.position;
+
+
             var forward = transform.forward * M.Dot(velocity, transform.forward);
-            var projected = velocity - forward;
-            velocity = forward + velocity * 2 + t.forward;
+            //var projected = velocity - forward;
+            //velocity = forward + velocity * 2 + t.forward;
             
             //var projected = velocity - transform.up * M.Dot(velocity, transform.up);
             var local = t.InverseTransformDirection(velocity);
-            var angle = -M.RadToDeg(Mathf.Atan2(local.z, local.x)) + 90;
+            //Debug.Log(local);
+            var angle = M.RadToDeg(Mathf.Atan2(local.x, local.z));
+            //if (local.z < 0)
+            //    angle = -angle;
+            var s = local.magnitude;
+            //if (s < 1f)
+            //    angle = M.Interpolate(0, angle, s);
             angle = Mathf.Repeat(angle+180, 360) - 180;
             angle = Mathf.Clamp(angle , -45,45);
 
@@ -40,6 +53,11 @@ public class RudderControl : MonoBehaviour
             transform.localEulerAngles = new Vector3(0, angle, 0);
         }
 
-        lastPosition = newPosition;
+        //lastPosition = newPosition;
+    }
+
+    public void UpdateIntention(ProjectedMotionSpace intention)
+    {
+        Intention = intention;
     }
 }
