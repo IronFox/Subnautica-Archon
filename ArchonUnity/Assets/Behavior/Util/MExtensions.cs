cb6b839778fdbc607fcc24d94d763869f5e628b8
IronFox <github@portal.delta-works.org>
2025-03-27 15:20:59 +0100

    Rework friction and rotation behavior

---
+++ ArchonUnity/Assets/Behavior/Util/MExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MExtensions
+{
+    public static Vector3 Combine(this Vector3 a, Vector3 b, Func<float, float, float> combiner)
+    {
+        return M.V3(
+            combiner(a.x, b.x),
+            combiner(a.y, b.y),
+            combiner(a.z, b.z)
+            );
+    }
+    public static Vector3 Combine(this Vector3 a, Vector3 b, Vector3 c, Func<float, float, float, float> combiner)
+    {
+        return M.V3(
+            combiner(a.x, b.x, c.x),
+            combiner(a.y, b.y, c.y),
+            combiner(a.z, b.z, c.z)
+            );
+    }
+}
