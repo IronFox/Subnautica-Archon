9fad08fd666e7f350c04f751eefa8962869cb02f
IronFox <github@portal.delta-works.org>
2025-03-28 09:56:37 +0100

    Reimport fbx with better animation. Add bay open/close animation

---
+++ ArchonUnity/Assets/Behavior/Components/BayControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BayControl : MonoBehaviour
+{
+    public float secondsToOpen = 3;
+    public bool open;
+    private float progress = 0;
+    private new Animation animation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open)
+            progress += Time.deltaTime / secondsToOpen;
+        else
+            progress -= Time.deltaTime / secondsToOpen;
+        progress = M.Saturate(progress);
+
+        if (!animation.isPlaying)
+            animation.Play();
+        foreach (AnimationState state in animation)
+        {
+            state.normalizedTime = progress;
+        }
+    }
+}
