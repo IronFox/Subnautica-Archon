ba4eaaf5b62ac577833f54d644090671f7ea2a95
IronFox <github@portal.delta-works.org>
2025-03-29 17:48:36 +0100

    Fix scripts

--- Scripts/bake.bat
+++ Scripts/bake.bat
@@ -1,15 +1,15 @@
 set "buildPath=..\..\BuildTarget"
 
-del /Q echelon.zip
-rmdir /Q /S .\Echelon
-mkdir .\Echelon
-mkdir .\Echelon\images
-mkdir .\Echelon\Localization
-copy /Y "%buildPath%\Subnautica Echelon_Data\Managed\EchelonScripts.dll" .\Echelon
-copy /Y "..\Unity\Assets\AssetBundles\Windows\echelon" .\Echelon
-copy /Y "..\Unity\Assets\AssetBundles\OSX\echelon" .\Echelon\echelon.osx
-copy /Y "..\Plugin\bin\Release\net4.7.2\Subnautica Echelon.dll" .\Echelon
-copy /Y "..\images\*.*" .\Echelon\images
-copy /Y "..\Localization\*.*" ".\Echelon\Localization"
+del /Q Archon.zip
+rmdir /Q /S .\Archon
+mkdir .\Archon
+mkdir .\Archon\images
+mkdir .\Archon\Localization
+copy /Y "%buildPath%\Subnautica Archon_Data\Managed\ArchonScripts.dll" .\Archon
+copy /Y "..\ArchonUnity\Assets\AssetBundles\Windows\archon" .\Archon
+copy /Y "..\ArchonUnity\Assets\AssetBundles\OSX\archon" .\Archon\archon.osx
+copy /Y "..\ArchonPlugin\bin\Release\net4.7.2\Subnautica Archon.dll" .\Archon
+copy /Y "..\images\*.*" .\Archon\images
+copy /Y "..\Localization\*.*" ".\Archon\Localization"
 
-powershell Compress-Archive .\Echelon echelon.zip
+powershell Compress-Archive .\Archon archon.zip
