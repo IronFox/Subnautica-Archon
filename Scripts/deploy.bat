340027cab66ffbdf103a12a01922010ffbfafbc7
IronFox <github@portal.delta-works.org>
2025-08-09 09:23:07 +0200

    Stop copying publicized as this appears to not be needed

--- Scripts/deploy.bat
+++ Scripts/deploy.bat
@@ -14,6 +14,6 @@ copy /Y "..\images\*.*" "%installPath%\BepInEx\plugins\Archon\images"
 mkdir "%installPath%\BepInEx\plugins\Archon\Localization"
 copy /Y "..\Localization\*.*" "%installPath%\BepInEx\plugins\Archon\Localization"
 copy /Y "%avsPath%\AVS\bin\Release\net4.7.2\AVS.dll" "%installPath%\BepInEx\plugins\Archon"
-copy /Y "%avsPath%\AVS\bin\Release\net4.7.2\Assembly-CSharp_publicized.dll" "%installPath%\BepInEx\plugins\Archon"
+rem copy /Y "%avsPath%\AVS\bin\Release\net4.7.2\Assembly-CSharp_publicized.dll" "%installPath%\BepInEx\plugins\Archon"
 
 rem "%installPath%\Subnautica.exe"
