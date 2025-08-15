5bf9c4c3349b365b5130ebf486e205ad05879e1b
IronFox <github@portal.delta-works.org>
2025-08-15 20:38:57 +0200

    Adapt to AVS changes

--- ArchonPlugin/MainPatcher.cs
+++ ArchonPlugin/MainPatcher.cs
@@ -38,7 +38,7 @@ namespace Subnautica_Archon
 
         public override string PluginId => PluginInfo.PLUGIN_GUID;
 
-        public override string ClassPrefix => "Archon";
+        public override string ModName => "Archon";
 
         public override void Awake()
         {
