ab06dc451b606a196b6c236d1583f11f4965715e
IronFox <github@portal.delta-works.org>
2025-08-17 10:35:29 +0200

    Change plugin ID to reflect 2023 branch

--- ArchonPlugin/PluginInfo.cs
+++ ArchonPlugin/PluginInfo.cs
@@ -2,7 +2,7 @@
 {
     public static class PluginInfo
     {
-        public const string PLUGIN_GUID = "com.ironfox.subnautica.archon.mod";
+        public const string PLUGIN_GUID = "com.ironfox.subnautica.2023.archon.mod";
         public const string PLUGIN_NAME = "Archon";
         public const string PLUGIN_VERSION = "1.0.3";
     }
