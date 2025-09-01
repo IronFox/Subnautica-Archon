e2756814661ef763bf5fb90f5f104425dea08545
IronFox <github@portal.delta-works.org>
2025-09-01 19:40:00 +0200

    Increment version number

--- ArchonPlugin/PluginInfo.cs
+++ ArchonPlugin/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string PLUGIN_GUID = "com.ironfox.subnautica.archon.mod";
         public const string PLUGIN_NAME = "Archon";
-        public const string PLUGIN_VERSION = "1.2.0";
+        public const string PLUGIN_VERSION = "1.2.1";
     }
 }
